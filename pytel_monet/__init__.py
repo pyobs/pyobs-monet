32d0e6719d3bf5346fabd9a9738acff7132274ca
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-03-04 19:53:33 +0000

    removed missing imports

--- pytel_monet/__init__.py
+++ pytel_monet/__init__.py
@@ -1,6 +1,1 @@
 from .bonnshutter import BonnShutter
-from .flatfielder import FlatFielder
-from .imagedb import ImageDB
-from .newimagewatcher import MonetImageWatcher
-from .roof import Roof
-from .stellaweather import StellaWeather
