e0ae1c08c5335475fa7d819e894ed1e6ab569fb7
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-03-28 21:19:06 +0100

    updated fits headers

--- pyobs_monet/mastermind.py
+++ pyobs_monet/mastermind.py
@@ -11,12 +11,14 @@ class MasterMind(PyObsModule, IFitsHeaderProvider):
         PyObsModule.__init__(self, *args, **kwargs)
         self._objid = 1000
         self._expid = 0
+        self._object = None
 
     def run(self):
         # get camera
         camera = self.comm['camera']    # type: ICamera, Proxy
 
         # take 10 bias images
+        self._object = 'bias'
         for self._expid in range(10):
             # take image
             log.info('Take image %d...', self._expid)
@@ -26,4 +28,8 @@ class MasterMind(PyObsModule, IFitsHeaderProvider):
         # build obj id
         objid = self.environment.night_obs().strftime('%Y%m%d') + 'S-%04d' % self._objid
         # return it
-        return {'OBJID': objid, 'EXPID': self._expid}
+        return {
+            'OBJID': objid,
+            'EXPID': self._expid,
+            'OBJECT': self._object
+        }
