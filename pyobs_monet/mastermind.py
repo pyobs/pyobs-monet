2d5744f5f2106cf954a13bc4a0ffa4bcb7802b74
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-03-28 17:39:37 +0100

    added simple mastermind

---
+++ pyobs_monet/mastermind.py
@@ -0,0 +1,29 @@
+import logging
+
+from pyobs import PyObsModule
+from pyobs.interfaces import IFitsHeaderProvider, ICamera
+
+log = logging.getLogger(__name__)
+
+
+class MasterMind(PyObsModule, IFitsHeaderProvider):
+    def __init__(self, *args, **kwargs):
+        PyObsModule.__init__(self, *args, **kwargs)
+        self._objid = 900
+        self._expid = 1
+
+    def run(self):
+        # get camera
+        camera = self.comm['camera']    # type: ICamera, Proxy
+
+        # take 10 bias images
+        for self._expid in range(1, 11):
+            # take image
+            log.info('Take image %d...', self._expid)
+            camera.expose(0, ICamera.ImageType.BIAS)
+
+    def get_fits_headers(self, *args, **kwargs) -> dict:
+        # build obj id
+        objid = self.environment.night_obs().strftime('%Y%m%d') + 'S%04d' % self._objid
+        # return it
+        return {'OBJID': objid, 'EXPID': self._expid}
