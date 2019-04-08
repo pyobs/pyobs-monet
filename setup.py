2faf7362542f37f7b4c8525af4f332b1125ab003
Tim-Oliver Husser <thusser@uni-goettingen.de>
2019-04-08 18:31:50 +0200

    changed version number to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='pyobs-monet',
-    version='0.1',
+    version='0.2',
     packages=['pyobs_monet'],
     author='Tim-Oliver Husser',
     author_email='husser@astro.physik.uni-goettingen.de',
