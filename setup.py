ad5fed4c462545ebca44f5def38a7c6ffa651222
Tim-Oliver Husser <thusser@uni-goettingen.de>
2020-12-17 16:23:06 +0100

    removed weather

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='pyobs-monet',
     version='0.2',
-    packages=['pyobs_monet', 'pyobs_monet.weather'],
+    packages=['pyobs_monet'],
     author='Tim-Oliver Husser',
     author_email='husser@astro.physik.uni-goettingen.de',
     description='pyobs modules for MONET'
